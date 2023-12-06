aa501148d16cf560b29a17a46ee15c1c60e5210f
Bishnu <jasbis2001@gmail.com>
2023-12-06 10:34:50 +0530

    portfolio components change

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -17,17 +17,17 @@ const Portfolio = () => {
           <div  className='portfolio_item-image'>
             <img src={IMG1} alt='pwd'/>
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>This is my Portfolio</h3>
           <div className='portfolio_item-cta'>
-          <a href='https://github.com/bishnu2001/weatherapp' target='_main' className='btn'>Github</a>
-          <a href='https://weatherbishnu.netlify.app/' target='_main' className='btn btn-primary'>Live Demo</a>
+            <a href='https://github.com/bishnu2001/Bishnuportfolio' target='_main' className='btn'>Github</a>
+            <a href='https://bishnuportfolio-vkl7.vercel.app/' target='_main' className='btn btn-primary'>Live Demo</a>
           </div>
         </article>
         <article className='portfolio_item'>
           <div  className='portfolio_item-image'>
             <img src={IMG2} alt='pwd'/>
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>This is my Weather App</h3>
           <div className='portfolio_item-cta'>
           <a href='https://github.com/bishnu2001/weatherapp' target='_main' className='btn'>Github</a>
           <a href='https://weatherbishnu.netlify.app/' target='_main' className='btn btn-primary'>Live Demo</a>
@@ -37,10 +37,10 @@ const Portfolio = () => {
           <div  className='portfolio_item-image'>
             <img src={IMG3} alt='pwd'/>
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>Google UI</h3>
           <div className='portfolio_item-cta'>
-          <a href='https://github.com/bishnu2001/weatherapp' target='_main' className='btn'>Github</a>
-          <a href='https://weatherbishnu.netlify.app/' target='_main' className='btn btn-primary'>Live Demo</a>
+            <a href='https://github.com/bishnu2001/googleclone' target='_main' className='btn'>Github</a>
+            <a href='https://googleclone-hnp3.vercel.app/' target='_main' className='btn btn-primary'>Live Demo</a>
           </div>
         </article>
 
